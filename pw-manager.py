148e86a889cb06598ab73f1c4e1c58133139da56
HugoSAINSON <hugo.sainson@etu.univ-orleans.fr>
2023-03-22 11:26:23 +0100

    copying on the paperclip the passwords

--- pw-manager.py
+++ pw-manager.py
@@ -1,6 +1,7 @@
 import encryptPassword.api as api
 import os
 import generatePassword.generator as generator
+import pyperclip
 
 def affiche_menu(options):
     """Print un menu avec comme option les arguments
@@ -19,8 +20,10 @@ def affiche_menu(options):
     print('╚════'+"═"*maxi+'════╝')
 
 def main():
+
     key_crypte = api.get_key()
     key = ' '
+
     while True:
         key = input('Quel est la clé ? ')
         if api.crypte_key(key) != key_crypte:
@@ -28,16 +31,20 @@ def main():
         else:
             print('Mot de passe correct')
             break
+
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         affiche_menu(['Ajouter un mot de passe','Générer un mot de passe','Chercher un mot de passe','Quitter'])
         option = input('Que souhaitez vous faire ? ')
+
         if option.isdigit() and 0<int(option)<5:
             option=int(option)
+
             if option == 1:
                 website = input('Quel est le site ? ')
                 pwd = api.crypte_pwd(input('Quel est le mot de passe ? '),key)
                 api.write_pwd(website,pwd)
+
             elif option == 2:
                 carac = " "
                 while type(carac) is str:
@@ -48,7 +55,10 @@ def main():
                 num = input("Autorisez vous les chiffres ? (y/n) ") != "n"
                 spe = input("Autorisez vous les caractère spéciaux ? (y/n) ") == "y"
                 space = input("Autorisez vous les espaces ? (y/n) ") =="y"
-                print (generator.generator(carac,up,num,spe,space))
+                pyperclip.copy(generator.generator(carac,up,num,spe,space))
+                print(pyperclip.paste())
+                print('le mot de passe à été copier dans le presse papier')
+
             elif option == 3:
                 website = input('Quel est le site ? ')
                 pwd = api.uncrypte_pwd(api.get_pwd(website),key)
@@ -56,8 +66,10 @@ def main():
                     print('Aucun mot de passe pour ce site ')
                 else:
                     print(pwd)
+
             elif option == 4:
                 break
+
         input('Entrer pour continuer ')
 
 main()
